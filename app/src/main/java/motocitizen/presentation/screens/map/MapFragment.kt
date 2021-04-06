13daef03b7c73de40158fdbf3432357457dc6bb6
Trunker2010 <Trunker2010@users.noreply.github.com>
2021-04-06 18:01:34 +0300

    добавлен LocListener, проверка разрешения локации

--- app/src/main/java/motocitizen/presentation/screens/map/MapFragment.kt
+++ app/src/main/java/motocitizen/presentation/screens/map/MapFragment.kt
@@ -1,8 +1,21 @@
 package motocitizen.presentation.screens.map
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_map.*
 import motocitizen.domain.lcenstate.isContent
@@ -15,9 +28,62 @@ import timber.log.Timber
 
 @AndroidEntryPoint
 class MapFragment : VMFragment<MapViewModel>(R.layout.fragment_map) {
+    private val REQST_CODE = 100
+
     override val viewModel: MapViewModel by viewModels()
 
     override fun initUi(savedInstanceState: Bundle?) {
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initViewModel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startLocation()
+
+
+    }
+
+    private fun startLocation() {
+        val permissionStatus = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            viewModel.starLocationUpdate()
+            viewModel.locationPoint.observe(this, {
+                Log.d("startLocation", "latitude= ${it.latitude}, longitude = ${it.longitude}")
+            })
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQST_CODE
+            )
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQST_CODE -> {
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    startLocation()
+                }
+            }
+        }
+
+
     }
 
     override fun initViewModel() {
@@ -27,7 +93,14 @@ class MapFragment : VMFragment<MapViewModel>(R.layout.fragment_map) {
             view_panel.isVisible = it.isContent()
             it.asContentOrNull()?.let(::renderContent)
         }
+
+        val locationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        viewModel.onAfterInit(locationManager)
+
+
     }
+
 
     private fun renderContent(list: List<Accident>) {
         Timber.i(list.size.toString())
--- app/src/main/java/motocitizen/presentation/screens/map/MapViewModel.kt
+++ app/src/main/java/motocitizen/presentation/screens/map/MapViewModel.kt
@@ -1,29 +1,53 @@
 package motocitizen.presentation.screens.map
 
+import android.annotation.SuppressLint
+import android.location.Criteria
+import android.location.LocationManager
+import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import motocitizen.domain.lcenstate.LcenState
 import motocitizen.domain.lcenstate.toLcenEventObservable
 import motocitizen.domain.model.accident.Accident
+import motocitizen.data.gps.LocListener
+import motocitizen.data.gps.LocationPoint
 import motocitizen.domain.usecases.AccidentUseCase
 import motocitizen.presentation.base.viewmodel.BaseViewModel
+
+
+
 
 class MapViewModel @ViewModelInject constructor(
     private val getAccidentUseCase: AccidentUseCase
 ) : BaseViewModel() {
+
+
+    val locationPoint = MutableLiveData<LocationPoint>()
+    lateinit var locationManager: LocationManager
+
+
+    fun onAfterInit(locManager: LocationManager) {
+        locationManager = locManager
+    }
+
 
     //todo убрать после реализации входных параметров
     private val token: String = "sasdacxc"
     private val depth: Int = 5
     //todo убрать после реализации входных параметров
 
+    private val locationListener = LocListener()
+
+
     private val _loadAccidentListState = MutableLiveData<LcenState<List<Accident>>>(LcenState.None)
     val loadAccidentListState: LiveData<LcenState<List<Accident>>>
         get() = _loadAccidentListState
 
+
     init {
         loadData()
+
     }
 
 
@@ -37,4 +61,16 @@ class MapViewModel @ViewModelInject constructor(
                 )
         }
     }
+
+
+    @SuppressLint("MissingPermission")
+    fun starLocationUpdate() {
+        locationListener.setLivedata(locationPoint)
+        val provider: String? = locationManager.getBestProvider(Criteria(), true)
+
+        locationManager.requestLocationUpdates(provider!!, 1000L, 1f, locationListener)
+        Log.d("starLocationUpdate", locationManager.hashCode().toString())
+
+    }
+
 }
